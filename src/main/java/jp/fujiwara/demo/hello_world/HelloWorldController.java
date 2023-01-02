9e5c96560c51575884695b4b558c0845f07f9b24
atahatah <ratdotl.hata@gmail.com>
2023-01-02 20:23:51 +0900

    hello, world

---
+++ src/main/java/jp/fujiwara/demo/hello_world/HelloWorldController.java
@@ -0,0 +1,12 @@
+package jp.fujiwara.demo.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, world!";
+    }
+}
