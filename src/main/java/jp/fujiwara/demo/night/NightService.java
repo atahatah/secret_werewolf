7a4e629fc22560c56380e55ae163315633dbc163
atahatah <ratdotl.hata@gmail.com>
2023-01-06 12:00:43 +0900

    update readme

--- src/main/java/jp/fujiwara/demo/night/NightService.java
+++ src/main/java/jp/fujiwara/demo/night/NightService.java
@@ -3,7 +3,6 @@ package jp.fujiwara.demo.night;
 import org.springframework.stereotype.Service;
 
 import jp.fujiwara.demo.global.GameState;
-import jp.fujiwara.demo.noon.NoonService;
 import jp.fujiwara.demo.parent_child.ParentService;
 import lombok.RequiredArgsConstructor;
 
